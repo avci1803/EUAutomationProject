04da2fecd93241bf0f7a1d70400689703d2c8c4a
avci1803 <mehmetavci1803@gmail.com>
2021-06-12 01:28:59 +0200

    added more hello feature

--- MavenProject/src/main/java/Hello.java
+++ MavenProject/src/main/java/Hello.java
@@ -3,5 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Developing future ");
+        System.out.println("Local repo is already published to Github");
+        System.out.println("Doing more work");
     }
 }
