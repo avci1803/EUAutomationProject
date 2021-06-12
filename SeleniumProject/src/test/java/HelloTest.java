ef4a51dc680f83df526afd755b48993348d2aa38
avci1803 <mehmetavci1803@gmail.com>
2021-06-12 02:22:02 +0200

    added second update

--- SeleniumProject/src/test/java/HelloTest.java
+++ SeleniumProject/src/test/java/HelloTest.java
@@ -2,5 +2,6 @@ public class HelloTest {
 
     public static void main(String[] args) {
         System.out.println("Helo Test");
+        System.out.println("About the share project on GitHub");
     }
 }
