9a745282bc1a2ac8db40e52a27106ef7842ed7dd
avci1803 <mehmetavci1803@gmail.com>
2021-06-12 03:09:51 +0200

    work 1

--- SeleniumProject/src/test/java/HelloTest.java
+++ SeleniumProject/src/test/java/HelloTest.java
@@ -3,5 +3,7 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Helo Test");
         System.out.println("About the share project on GitHub");
+
+        //work1
     }
 }
